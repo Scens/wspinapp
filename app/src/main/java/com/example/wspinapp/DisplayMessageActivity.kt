a43223cbec2893ae56a40ea31918dd51d432c688
MijPeter <michal.kuba.jankowski@gmail.com>
2022-09-27 22:52:27 +0200

    create a second activity

---
+++ app/src/main/java/com/example/wspinapp/DisplayMessageActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wspinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+    }
+}
