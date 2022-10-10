2a1324adb508690025f4c7c1279ef66c5275e996
MijPeter <michal.kuba.jankowski@gmail.com>
2022-10-11 00:01:05 +0200

    add button that overrides important image with selected image from images folder on phone

--- app/src/main/java/com/example/wspinapp/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/wspinapp/DisplayMessageActivity.kt
@@ -1,6 +1,8 @@
 package com.example.wspinapp
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,12 +11,24 @@ class DisplayMessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
 
+        setTextView()
+        setImageView()
+    }
+
+    private fun setTextView() {
         // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(EXTRA_MESSAGE)
 
         // Capture the layout's TextView and set the string as its text
         findViewById<TextView>(R.id.textView).apply {
             text = message
+        }
+    }
+
+    private fun setImageView() {
+        val imageUri: String? = intent.getStringExtra(IMAGE_URI)
+        if (imageUri?.isBlank() == false) {
+            findViewById<ImageView>(R.id.imageView3).setImageURI(Uri.parse(imageUri))
         }
     }
 }
--- app/src/main/java/com/example/wspinapp/MainActivity.kt
+++ app/src/main/java/com/example/wspinapp/MainActivity.kt
@@ -1,27 +1,46 @@
 package com.example.wspinapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
 
 const val EXTRA_MESSAGE = "com.example.wspinapp.MESSAGE"
+const val IMAGE_URI = "com.example.wspinapp.IMAGE_URI"
 
 class MainActivity : AppCompatActivity() {
+    private var imageUri: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectButton = findViewById<Button>(R.id.button3)
+
+        selectButton.setOnClickListener {
+            getContent.launch("image/*")
+        }
     }
 
     /** Called when the user taps the Send button */
     fun sendMessage(view: View) {
+        println(imageUri)
         val editText = findViewById<EditText>(R.id.textBox)
         val message = editText.text.toString()
         val intent = Intent(this, DisplayMessageActivity::class.java).apply {
             putExtra(EXTRA_MESSAGE, message)
+            if (imageUri?.isNotEmpty() == true) {
+                putExtra(IMAGE_URI, imageUri.toString())
+            }
         }
         startActivity(intent)
     }
 
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        imageUri = uri.toString()
+    }
 }
