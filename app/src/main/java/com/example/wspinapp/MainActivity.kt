6ad14ae6a91431e58c0657df327e1ca4f6364614
Michal Jankowski <37120577+MijPeter@users.noreply.github.com>
2022-10-12 22:28:42 +0200

    add button that overrides important image with selected image from im… (#13)
    * add separate view for walls images and remove test features

--- app/src/main/java/com/example/wspinapp/MainActivity.kt
+++ app/src/main/java/com/example/wspinapp/MainActivity.kt
@@ -1,31 +1,19 @@
 package com.example.wspinapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-
-const val EXTRA_MESSAGE = "com.example.wspinapp.MESSAGE"
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    /** Called when the user taps the Send button */
-    fun sendMessage(view: View) {
-        val editText = findViewById<EditText>(R.id.textBox)
-        val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE, message)
-        }
-        startActivity(intent)
-    }
-
-    fun watchVideo(view: View) {
-        val intent = Intent(this, WatchVideoActivity::class.java)
+    fun showImages(view: View) {
+        val intent = Intent(this, ImagesActivity::class.java)
         startActivity(intent)
     }
 }
