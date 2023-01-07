fdbff598fd0661a54fba565567901ed35defb062
Michal Jankowski <37120577+MijPeter@users.noreply.github.com>
2023-01-07 20:53:46 +0100

    Development (#25)
    * - added gradlew task runTests
    - modified github actions to run runTests
    - upgraded gradlew plugin version
    
    * - added gradlew task runTests
    - modified github actions to run runTests
    - upgraded gradlew plugin version
    
    * incremented compileSdkVersion
    
    * create a second activity
    
    * https://developer.android.com/training/basics/firstapp/starting-activity
    
    * added important image
    
    * added watch video activity
    
    * add logo (#9)
    
    * remove existing .idea files
    
    * adjust gitignore to ignore all .idea files
    
    * add button that overrides important image with selected image from im… (#13)
    
    * add separate view for walls images and remove test features
    
    * add feature that changes color of background in main menu when clicked (#21)
    
    * update kotlin (#24)
    
    Co-authored-by: r.brzezinski <rafalbrzez@gmail.com>

--- app/src/main/java/com/example/wspinapp/MainActivity.kt
+++ app/src/main/java/com/example/wspinapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.wspinapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showImages(view: View) {
+        val intent = Intent(this, ImagesActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun changeBackgroundColor(view: View) {
+        val logoColors = resources.getIntArray(R.array.logo_colors)
+        val randomLogoColor = logoColors[Random.nextInt(logoColors.size)]
+        view.setBackgroundColor(randomLogoColor)
     }
 }
