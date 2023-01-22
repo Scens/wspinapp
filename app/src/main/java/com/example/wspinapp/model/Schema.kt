f7d29b021825892e5447c0f04d2599066d2fcdd6
Michał Jankowski <michal.kuba.jankowski@gmail.com>
2023-01-22 16:36:07 +0100

    WIP2

--- app/src/main/java/com/example/wspinapp/model/Schema.kt
+++ app/src/main/java/com/example/wspinapp/model/Schema.kt
@@ -24,7 +24,34 @@ data class Wall(
     val CreatedAt: String = "",
     val UpdatedAt: String = "",
     val DeletedAt: String = "",
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Wall
+
+        if (!Holds.contentEquals(other.Holds)) return false
+        if (ImageUrl != other.ImageUrl) return false
+        if (ID != other.ID) return false
+        if (CreatedAt != other.CreatedAt) return false
+        if (UpdatedAt != other.UpdatedAt) return false
+        if (DeletedAt != other.DeletedAt) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = Holds.contentHashCode()
+        result = 31 * result + ImageUrl.hashCode()
+        result = 31 * result + ID.hashCode()
+        result = 31 * result + CreatedAt.hashCode()
+        result = 31 * result + UpdatedAt.hashCode()
+        result = 31 * result + DeletedAt.hashCode()
+        return result
+    }
+}
+
 data class AddWall(
     val Holds: Array<Hold>
 )
