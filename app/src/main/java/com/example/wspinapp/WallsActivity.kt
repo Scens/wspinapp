0735874c8049b9fd4dbc1f91764c3b2c5741e61a
Michał Jankowski <michal.kuba.jankowski@gmail.com>
2023-01-22 19:51:46 +0100

    Added fetchin of routes and showind them in recycler view

--- app/src/main/java/com/example/wspinapp/WallsActivity.kt
+++ app/src/main/java/com/example/wspinapp/WallsActivity.kt
@@ -2,10 +2,18 @@ package com.example.wspinapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.wspinapp.model.Wall
+import com.example.wspinapp.utils.backendClient
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 
 var invalid = false
@@ -15,11 +23,11 @@ var invalid = false
 class WallsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_walls)
+        setContentView(R.layout.walls)
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
 
         runBlocking {
-            val walls = Datasource().loadWalls()
+            val walls = backendClient.fetchWalls()
             recyclerView.adapter = WallAdapter(this, walls)
         }
     }
@@ -31,20 +39,53 @@ class WallsActivity : AppCompatActivity() {
         }
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         runBlocking {
-            val walls = Datasource().loadWalls()
+            val walls = backendClient.fetchWalls()
             recyclerView.adapter = WallAdapter(this, walls)
         }
     }
 
     fun openWall(view: View) {
         val wallId = view.findViewById<TextView>(R.id.wall_id).text.toString().toInt().toUInt()
-        val imageUrl = imageUrls[wallId]
-        val intent = Intent(this, WallActivity::class.java).putExtra(IMAGE_URL_MESSAGE, imageUrl)
+        val intent = Intent(this, WallActivity::class.java).putExtra(WALL_ID_MESSAGE, wallId.toString())
         startActivity(intent)
     }
 
     fun addWall(view: View) {
         val intent = Intent(this, AddWallActivity::class.java)
         startActivity(intent)
+    }
+}
+
+val walls: MutableMap<UInt, Wall> = mutableMapOf()
+
+class WallAdapter(
+    private val context: CoroutineScope,
+    private val dataset: List<Wall>
+) : RecyclerView.Adapter<WallAdapter.WallViewHolder>() {
+
+    class WallViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val wallView: ConstraintLayout = view.findViewById(R.id.wall_preview)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WallViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.wall_preview, parent, false)
+
+        return WallViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: WallViewHolder, position: Int) {
+        val item = dataset[position]
+        val textView: TextView = holder.wallView.getViewById(R.id.wall_id) as TextView
+        textView.text = item.ID.toString()
+
+        if (item.ImageUrl?.isBlank() == false) {
+            holder.wallView.findViewById<ImageView>(R.id.wall_image).load(item.ImageUrl)
+        }
+        walls[item.ID!!] = item
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
     }
 }
