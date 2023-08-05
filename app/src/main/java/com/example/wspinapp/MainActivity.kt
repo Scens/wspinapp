a8afccfc9ce600e045c02f7ceae8939c59b5efdb
Michał Jankowski <michal.kuba.jankowski@gmail.com>
2023-08-05 23:56:21 +0200

    add more error handling etc

--- app/src/main/java/com/example/wspinapp/MainActivity.kt
+++ app/src/main/java/com/example/wspinapp/MainActivity.kt
@@ -9,18 +9,55 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.wspinapp.utils.backendClient
+import io.ktor.client.network.sockets.*
+import io.ktor.client.plugins.*
 import kotlinx.coroutines.launch
+import java.net.ConnectException
 import kotlin.random.Random
 
+// Exception handling function
+private fun Context.handleException(e: Exception) {
+    when(e) {
+        is HttpRequestTimeoutException,
+        is ConnectTimeoutException,
+        is SocketTimeoutException,
+        is ConnectException -> {
+            Toast.makeText(
+                this,
+                "Connection error. Try again later.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        else -> {
+            Toast.makeText(
+                this,
+                "An unknown error occurred: ${e::class.simpleName}",
+                Toast.LENGTH_LONG
+            ).show()
+            Log.println(Log.ERROR, "application", e.stackTraceToString())
+        }
+    }
+}
+
+// Suspend function version
 suspend fun <T> Context.executeWithHandling(function: suspend () -> T?): T? {
     return try {
         function()
     } catch (e: Exception) {
-        Toast.makeText(this, "An unknown error occurred: ${e::class.simpleName}", Toast.LENGTH_LONG).show()
-        Log.println(Log.ERROR, "application", e.stackTraceToString())
+        handleException(e)
         null
     }
 }
+
+// Non-suspend function version
+fun Context.executeNonSuspendWithHandling(action: () -> Unit) {
+    try {
+        action()
+    } catch (e: Exception) {
+        handleException(e)
+    }
+}
+
 
 
 
@@ -35,18 +72,9 @@ class MainActivity : AppCompatActivity() {
         val activity = this
         lifecycleScope.launch {
             activity.executeWithHandling {
-                val dataset = backendClient.fetchWalls() // fetchWalls is a suspend function
-                if (dataset == null) {
-                    Toast.makeText(
-                        activity,
-                        "Connection error. Try again later.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else {
-                    val intent = Intent(activity, WallsActivity::class.java)
-                    intent.putParcelableArrayListExtra("dataset", ArrayList(dataset))
-                    startActivity(intent)
-                }
+                WallManager.fetchWalls() // fetchWalls is a suspend function
+                val intent = Intent(activity, WallsActivity::class.java)
+                startActivity(intent)
             }
 
         }
